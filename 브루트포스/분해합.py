e98c73286ff024537e215998e52ec847a6cc22e4
sjms321 <sjms321@naver.com>
2022-01-14 23:49:11 +0900

    브루트 포스 분해합

---
+++ 브루트포스/분해합.py
@@ -0,0 +1,11 @@
+n = int(input())
+result = 0
+
+for i in range(1,n+1):
+    listNum = list(map(int,str(i)))
+    result = i + sum(listNum)
+    if result == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
