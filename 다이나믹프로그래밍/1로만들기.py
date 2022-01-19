5e1550a5b424b8c2927419bf2014191175ef5a92
min_seok <sjms321@naver.com>
2022-01-19 21:42:07 +0900

    dp 1로만들기

--- 다이나믹프로그래밍/1로만들기.py
+++ 다이나믹프로그래밍/1로만들기.py
@@ -1,15 +1,13 @@
-x=int(input())
+n = int(input())
 
-d = [0] * 30001
+dp = [0] * (n+1)
 
-for i in range(2,x+1):
-    d[i]=d[i-1]+1
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
 
-    if i%2==0:
-        d[i]=min(d[i],d[i//2]+1)
-    if i%3==0:
-        d[i]=min(d[i],d[i//3]+1)
-    if i%5==0:
-        d[i]=min(d[i],d[i//5]+1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
 
-print(d[x])
+print(dp[n])
